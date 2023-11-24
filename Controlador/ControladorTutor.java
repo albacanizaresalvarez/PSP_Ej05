591befb72ab3d4338de4b2ed85bd1808c86e42da
Pablo Luis Rodríguez Gálvez <147171758+PabloRodriguez23@users.noreply.github.com>
2023-11-24 16:45:35 +0100

    Pablo, Consigo que funcione el metodo.

--- Controlador/ControladorTutor.java
+++ Controlador/ControladorTutor.java
@@ -6,6 +6,8 @@
 package Controlador;
 
 import Modelo.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -24,32 +26,45 @@ public class ControladorTutor {
     }
 
     // Método para obtener una colección de objetos Tutor
-    public Collection<Tutor> obtenerTutores() {
-        Collection<Tutor> tutores = new ArrayList<>();
 
-        try {
-            if (resultSet != null) {
-                // Iterar a través del ResultSet y construir objetos Tutor
-                while (resultSet.next()) {
-                    int Codtutor = resultSet.getInt("Codtutor");
-                    String nombre = resultSet.getString("nombre");
-                    String apellidos = resultSet.getString("apellidos");
-                    String fototutor = resultSet.getString("fototutor");
-                    float sueldo = resultSet.getFloat("sueldo");
-                    int a = resultSet.getInt("a");
-                    int m = resultSet.getInt("m");
-                    int d = resultSet.getInt("d");
+    /**
+     *
+     * @return
+     * @throws SQLException
+     */
+    static public Collection<Tutor> obtenerTutores() throws SQLException {
+    Collection<Tutor> tutores = new ArrayList<>();
+    try (Connection conexion = ControladorBD.obtenerConexion()) {
+        String sql = "SELECT Codtutor, nombre, apellidos, fototutor, sueldo, a, m, d FROM tutor";
 
-                    // Construir y agregar un objeto Tutor a la colección
-                    tutores.add(new Tutor(Codtutor, nombre, apellidos, fototutor, sueldo, a, m, d));
+        try (PreparedStatement declaracion = conexion.prepareStatement(sql)) {
+            try (ResultSet resultado = declaracion.executeQuery()) {
+                try {
+                    if (resultado != null) {
+                        // Iterar a través del ResultSet y construir objetos Tutor
+                        while (resultado.next()) {
+                            int Codtutor = resultado.getInt("Codtutor");
+                            String nombre = resultado.getString("nombre");
+                            String apellidos = resultado.getString("apellidos");
+                            String fototutor = resultado.getString("fototutor");
+                            float sueldo = resultado.getFloat("sueldo");
+                            int a = resultado.getInt("a");
+                            int m = resultado.getInt("m");
+                            int d = resultado.getInt("d");
+
+                            // Construir y agregar un objeto Tutor a la colección
+                            tutores.add(new Tutor(Codtutor, nombre, apellidos, fototutor, sueldo, a, m, d));
+                        }
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
                 }
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
 
-        return tutores;
+                return tutores;
+            }
+        }
     }
+}
 
     // Método para establecer el ResultSet asociado al JPanel "resumen"
     public void setResultSet(ResultSet resultSet) {
@@ -59,10 +74,10 @@ public class ControladorTutor {
     // Método para intercambiar dos objetos Tutor en la base de datos
     public void intercambiarTutores(Tutor tutor1, Tutor tutor2) {
 //        try {
-            if (resultSet != null) {
-                // Implementa la lógica para intercambiar los datos de tutor1 y tutor2 en la base de datos
-                // Puedes utilizar las sentencias SQL necesarias para realizar el intercambio.
-            }
+        if (resultSet != null) {
+            // Implementa la lógica para intercambiar los datos de tutor1 y tutor2 en la base de datos
+            // Puedes utilizar las sentencias SQL necesarias para realizar el intercambio.
+        }
 //        } catch (SQLException e) {
 //            e.printStackTrace();
 //        }
@@ -71,10 +86,10 @@ public class ControladorTutor {
     // Método para actualizar un objeto Tutor en la base de datos
     public void actualizarTutor(Tutor tutor) {
 //        try {
-            if (resultSet != null) {
-                // Implementa la lógica para actualizar los datos del tutor en la base de datos
-                // Puedes utilizar las sentencias SQL necesarias para realizar la actualización.
-            }
+        if (resultSet != null) {
+            // Implementa la lógica para actualizar los datos del tutor en la base de datos
+            // Puedes utilizar las sentencias SQL necesarias para realizar la actualización.
+        }
 //        } catch (SQLException e) {
 //            e.printStackTrace();
 //        }
