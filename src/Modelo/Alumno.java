e2ad546cb793add20a796a74354a1ec9b9733952
IreneMoya9 <147318656+IreneMoya9@users.noreply.github.com>
2023-12-05 12:00:00 +0100

    terminado

---
+++ src/Modelo/Alumno.java
@@ -0,0 +1,178 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author alumno
+ */
+@Entity
+@Table(name = "ALUMNO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Alumno_1.findAll", query = "SELECT a FROM Alumno_1 a")
+    , @NamedQuery(name = "Alumno_1.findByCodalum", query = "SELECT a FROM Alumno_1 a WHERE a.codalum = :codalum")
+    , @NamedQuery(name = "Alumno_1.findByNombrealum", query = "SELECT a FROM Alumno_1 a WHERE a.nombrealum = :nombrealum")
+    , @NamedQuery(name = "Alumno_1.findByEdadalum", query = "SELECT a FROM Alumno_1 a WHERE a.edadalum = :edadalum")
+    , @NamedQuery(name = "Alumno_1.findByFotoalum", query = "SELECT a FROM Alumno_1 a WHERE a.fotoalum = :fotoalum")
+    , @NamedQuery(name = "Alumno_1.findByNotamediaalum", query = "SELECT a FROM Alumno_1 a WHERE a.notamediaalum = :notamediaalum")
+    , @NamedQuery(name = "Alumno_1.findByFechanacimiento", query = "SELECT a FROM Alumno_1 a WHERE a.fechanacimiento = :fechanacimiento")})
+public class Alumno implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @Column(name = "CODALUM")
+    private Integer codalum;
+    @Column(name = "NOMBREALUM")
+    private String nombrealum;
+    @Column(name = "EDADALUM")
+    private Integer edadalum;
+    @Column(name = "FOTOALUM")
+    private String fotoalum;
+    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    @Column(name = "NOTAMEDIAALUM")
+    private Float notamediaalum;
+    @Column(name = "FECHANACIMIENTO")
+    @Temporal(TemporalType.DATE)
+    private Date fechanacimiento;
+    @JoinColumn(name = "CODTUTOR", referencedColumnName = "CODTUTOR")
+    @ManyToOne
+    private Tutor codtutor;
+
+    public Alumno() {
+    }
+
+    public Alumno(Integer codalum, String nombrealum, Integer edadalum, String fotoalum, Float notamediaalum, Date fechanacimiento) {
+        this.codalum = codalum;
+        this.nombrealum = nombrealum;
+        this.edadalum = edadalum;
+        this.fotoalum = fotoalum;
+        this.notamediaalum = notamediaalum;
+        this.fechanacimiento = fechanacimiento;
+    }
+    
+    public Alumno(Integer codalum, String nombrealum, Integer edadalum, Float notamediaalum, Date fechanacimiento) {
+        this.codalum = codalum;
+        this.nombrealum = nombrealum;
+        this.edadalum = edadalum;
+        this.notamediaalum = notamediaalum;
+        this.fechanacimiento = fechanacimiento;
+    }
+    
+    
+
+    public Alumno(Integer codalum) {
+        this.codalum = codalum;
+    }
+
+    public Alumno(int codalum, String nombrealum, int edadalum, float notamediaalum, Date fechanacimiento, String fotoalum) {
+          this.codalum = codalum;
+        this.nombrealum = nombrealum;
+        this.edadalum = edadalum;
+        this.fotoalum = fotoalum;
+        this.notamediaalum = notamediaalum;
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public Integer getCodalum() {
+        return codalum;
+    }
+
+    public void setCodalum(Integer codalum) {
+        this.codalum = codalum;
+    }
+
+    public String getNombrealum() {
+        return nombrealum;
+    }
+
+    public void setNombrealum(String nombrealum) {
+        this.nombrealum = nombrealum;
+    }
+
+    public Integer getEdadalum() {
+        return edadalum;
+    }
+
+    public void setEdadalum(Integer edadalum) {
+        this.edadalum = edadalum;
+    }
+
+    public String getFotoalum() {
+        return fotoalum;
+    }
+
+    public void setFotoalum(String fotoalum) {
+        this.fotoalum = fotoalum;
+    }
+
+    public Float getNotamediaalum() {
+        return notamediaalum;
+    }
+
+    public void setNotamediaalum(Float notamediaalum) {
+        this.notamediaalum = notamediaalum;
+    }
+
+    public Date getFechanacimiento() {
+        return fechanacimiento;
+    }
+
+    public void setFechanacimiento(Date fechanacimiento) {
+        this.fechanacimiento = fechanacimiento;
+    }
+
+    public Tutor getCodtutor() {
+        return codtutor;
+    }
+
+    public void setCodtutor(Tutor codtutor) {
+        this.codtutor = codtutor;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codalum != null ? codalum.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Alumno)) {
+            return false;
+        }
+        Alumno other = (Alumno) object;
+        if ((this.codalum == null && other.codalum != null) || (this.codalum != null && !this.codalum.equals(other.codalum))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Modelo.Alumno_1[ codalum=" + codalum + " ]";
+    }
+    
+}
---
+++ src/Vista/JPanelDetalle.java
@@ -0,0 +1,255 @@
+package Vista;
+
+import Controlador.*;
+import java.sql.SQLException;
+import Modelo.*;
+import java.awt.Image;
+import java.util.ArrayList;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+public final class JPanelDetalle extends javax.swing.JPanel {
+
+    ControladorDetalle ControladorDetalle;
+    ControladorCollection CA = new ControladorCollection();
+    ArrayList<Alumno> Alumnos;
+    ControladorDetalle calum = new ControladorDetalle();
+    int indice = 0;
+
+    public JPanelDetalle(Tutor t) throws SQLException {
+        initComponents();
+        Alumnos = CA.obteneralumnos();
+        btnAnterior.setEnabled(false);
+        mostrarDatosAlum(indice);
+    }
+
+    public void mostrarDatosAlum(int indice) {
+
+        txtCodigo.setText("" + Alumnos.get(indice).getCodalum());
+        txtNombre.setText(Alumnos.get(indice).getNombrealum());
+        txtEdad.setText("" + Alumnos.get(indice).getEdadalum());
+        txtNota.setText("" + Alumnos.get(indice).getNotamediaalum());
+        jDateChooser2.setDate(Alumnos.get(indice).getFechanacimiento());
+        ImageIcon image = new ImageIcon("src/Imagenes/" + Alumnos.get(indice).getFotoalum());
+        Icon icon = new ImageIcon(
+                image.getImage().getScaledInstance(130, 150, Image.SCALE_DEFAULT)
+        );
+        imagen.setIcon(icon);
+        this.repaint();
+
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        txtNombre = new javax.swing.JTextField();
+        txtEdad = new javax.swing.JTextField();
+        txtNota = new javax.swing.JTextField();
+        btnSiguiente = new javax.swing.JButton();
+        btnAnterior = new javax.swing.JButton();
+        btnGuardar = new javax.swing.JButton();
+        jLabel6 = new javax.swing.JLabel();
+        txtCodigo = new javax.swing.JTextField();
+        jDateChooser2 = new com.toedter.calendar.JDateChooser();
+        imagen = new javax.swing.JLabel();
+
+        jLabel1.setText("Nombre:");
+
+        jLabel2.setText("DATOS ALUMNOS");
+
+        jLabel3.setText("Edad:");
+
+        jLabel4.setText("Nota media:");
+
+        jLabel5.setText("Fecha de nacimiento:");
+
+        txtNombre.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNombreActionPerformed(evt);
+            }
+        });
+
+        txtEdad.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtEdadActionPerformed(evt);
+            }
+        });
+
+        txtNota.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNotaActionPerformed(evt);
+            }
+        });
+
+        btnSiguiente.setText("Siguiente");
+        btnSiguiente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSiguienteActionPerformed(evt);
+            }
+        });
+
+        btnAnterior.setText("Anterior");
+        btnAnterior.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnAnteriorActionPerformed(evt);
+            }
+        });
+
+        btnGuardar.setText("Guardar");
+
+        jLabel6.setText("Código:");
+
+        txtCodigo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtCodigoActionPerformed(evt);
+            }
+        });
+
+        imagen.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel2)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(28, 28, 28)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                .addComponent(jLabel4)
+                                .addComponent(jLabel5)
+                                .addComponent(jLabel3)
+                                .addComponent(jLabel1)
+                                .addComponent(jLabel6))
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(txtNota)
+                                .addComponent(txtEdad)
+                                .addComponent(txtNombre)
+                                .addComponent(txtCodigo)
+                                .addComponent(jDateChooser2, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addGroup(layout.createSequentialGroup()
+                            .addGap(47, 47, 47)
+                            .addComponent(btnAnterior)
+                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btnGuardar))))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(70, 70, 70)
+                        .addComponent(btnSiguiente)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 73, Short.MAX_VALUE)
+                        .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(26, 26, 26))))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(jLabel2)
+                .addGap(28, 28, 28)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel6)
+                            .addComponent(txtCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(txtNombre, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtEdad, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, 13, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(txtNota, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, 16, Short.MAX_VALUE)
+                                .addGap(42, 42, 42))
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jDateChooser2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(btnSiguiente)
+                            .addComponent(btnAnterior)
+                            .addComponent(btnGuardar))
+                        .addGap(59, 59, 59))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(imagen, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void txtNombreActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNombreActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtNombreActionPerformed
+
+    private void txtEdadActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtEdadActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtEdadActionPerformed
+
+    private void txtNotaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNotaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtNotaActionPerformed
+
+    private void txtCodigoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtCodigoActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtCodigoActionPerformed
+
+    private void btnAnteriorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAnteriorActionPerformed
+        btnSiguiente.setEnabled(true);
+        btnAnterior.setEnabled(true);
+        if (indice == 1) {
+            btnAnterior.setEnabled(false);
+            btnSiguiente.setEnabled(true);
+        }
+
+        indice--;
+        mostrarDatosAlum(indice);
+    }//GEN-LAST:event_btnAnteriorActionPerformed
+
+    private void btnSiguienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSiguienteActionPerformed
+        btnSiguiente.setEnabled(true);
+        btnAnterior.setEnabled(true);
+        if (indice + 2 == Alumnos.size()) {
+            btnAnterior.setEnabled(true);
+            btnSiguiente.setEnabled(false);
+        }
+
+        indice++;
+        mostrarDatosAlum(indice);
+    }//GEN-LAST:event_btnSiguienteActionPerformed
+
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnAnterior;
+    private javax.swing.JButton btnGuardar;
+    private javax.swing.JButton btnSiguiente;
+    private javax.swing.JLabel imagen;
+    private com.toedter.calendar.JDateChooser jDateChooser2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JTextField txtCodigo;
+    private javax.swing.JTextField txtEdad;
+    private javax.swing.JTextField txtNombre;
+    private javax.swing.JTextField txtNota;
+    // End of variables declaration//GEN-END:variables
+
+}
