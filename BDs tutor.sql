557e2fc95efc994de18edb6d77260381e3845232
albacanizaresalvarez <147172673+albacanizaresalvarez@users.noreply.github.com>
2023-12-05 15:34:44 +0100

    Update BDs tutor.sql

--- BDs tutor.sql
+++ BDs tutor.sql
@@ -24,18 +24,18 @@ INSERT INTO tutor VALUES (1028,'Irene','Moya','tutor4.jpg',1500,'06/29/2020', 't
 INSERT INTO tutor VALUES (1029,'Tomas','Garcia','tutor2.jpg',2100,'07/15/2020', 'tutoralumno');
 INSERT INTO tutor VALUES (1030,'Juan','Canizares','tutor2.jpg',2500,'10/15/2021', 'tutoralumno');
 
-INSERT INTO alumno VALUES (101,'Manuel', 23, 'alumno1.jpg',9.5 , '10/17/2018', 1027);
-INSERT INTO alumno VALUES (102,'Antonio', 24,'alumno2.jpg', 2.9, '11/01/2019', 1028);
-INSERT INTO alumno VALUES (103,'Gonzalo', 26,'alumno5.jpg',9, '01/05/2019', 1026);
-INSERT INTO alumno VALUES (104,'Sebastian', 18,'alumno2.jpg', 4.5,'08/14/2019', 1027);
-INSERT INTO alumno VALUES (105,'Amalia', 21,'alumno4.jpg', 5.6, '06/20/2021', 1028);
-INSERT INTO alumno VALUES (106,'Manuel', 27,'alumno1.jpg',8.4,'05/28/2021', 1028);
-INSERT INTO alumno VALUES (107,'Sabrina', 28,'alumno3.jpg',8,'03/10/2019', 1026);
-INSERT INTO alumno VALUES (108,'Pascual', 26,'alumno2.jpg',7.2,'07/11/2020', 1026);
-INSERT INTO alumno VALUES (109,'Luna', 27,'alumno3.jpg',8.3,'06/12/2020',1028);
-INSERT INTO alumno VALUES (110,'Mario', 20,'alumno5.jpg',3.5,'11/21/2021', 1028);
-INSERT INTO alumno VALUES (109,'Luna', 27,'alumno3.jpg',8.3,'06/12/2020',1030);
-INSERT INTO alumno VALUES (110,'Mario', 20,'alumno5.jpg',3.5,'11/21/2021', 1029);
+INSERT INTO alumno VALUES (101,'Manuel', 3, 'alumno1.jpg',9.5 , '10/17/2018', 1027);
+INSERT INTO alumno VALUES (102,'Antonio', 4,'alumno2.jpg', 2.9, '11/01/2019', 1028);
+INSERT INTO alumno VALUES (103,'Gonzalo', 6,'alumno5.jpg',9, '01/05/2019', 1026);
+INSERT INTO alumno VALUES (104,'Sebastian', 8,'alumno2.jpg', 4.5,'08/14/2019', 1027);
+INSERT INTO alumno VALUES (105,'Amalia', 2,'alumno4.jpg', 5.6, '06/20/2021', 1028);
+INSERT INTO alumno VALUES (106,'Manuel', 7,'alumno1.jpg',8.4,'05/28/2021', 1028);
+INSERT INTO alumno VALUES (107,'Sabrina', 8,'alumno3.jpg',8,'03/10/2019', 1026);
+INSERT INTO alumno VALUES (108,'Pascual', 6,'alumno2.jpg',7.2,'07/11/2020', 1026);
+INSERT INTO alumno VALUES (109,'Luna', 4,'alumno3.jpg',8.3,'06/12/2020',1028);
+INSERT INTO alumno VALUES (110,'Mario', 3,'alumno5.jpg',3.5,'11/21/2021', 1028);
+INSERT INTO alumno VALUES (109,'Luna', 5,'alumno3.jpg',8.3,'06/12/2020',1030);
+INSERT INTO alumno VALUES (110,'Mario', 5,'alumno5.jpg',3.5,'11/21/2021', 1029);
 
 SELECT *
 FROM tutor;
